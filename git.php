214edd2622c84ffcb8546397d288e49b7a07ba32
Samson Oyango <samson.oyango@strathmore.edu>
2018-07-06 17:19:29 +0300

    Added two parameters and initilized them. Then I outputed their values

--- git.php
+++ git.php
@@ -1,3 +1,9 @@
 <?php 
-	echo "WELCOME";
+
+
+	$data_1 = "Simeon";
+	$valu   = 1200;
+	echo 'This is the owner of the item '.$data_1.".".'<br>';
+	echo "This is the amount he paid ".$valu."."."<br>";
+	// echo "WELCOME";
  ?>
